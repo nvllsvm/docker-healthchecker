c34cc595ad201b73958a5a924d5c0360945e460e
Andrew Rabert <ar@nullsum.net>
2021-01-25 18:59:03 -0500

    Release 0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# Changelog
+## 0.2.0
+- Output which containers failed to become healthy when timeout is exceeded
--- docker_healthchecker.py
+++ docker_healthchecker.py
@@ -6,7 +6,7 @@ import logging
 import subprocess
 import sys
 
-version = '0.1.0'
+version = '0.2.0'
 
 _LOGGER = logging.getLogger('docker-healthchecker')
 
